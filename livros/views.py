12bc5a1151b8c478ae2e5ebf073dd2e5d4a12df1
Iran-Ferreira <iranferreira712@gmail.com>
2026-05-22 10:05:09 -0300

    tokens inseridos nas views

--- livros/views.py
+++ livros/views.py
@@ -1,7 +1,15 @@
-import json
-from django.shortcuts import render
-from django.http import JsonResponse
-from django.views.decorators.csrf import csrf_exempt
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import permission_classes
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def rota_protegida(request):
+
+    return Response({
+        "mensagem": "Usuário autenticado com sucesso"
+    })
 
 # Create your views here.
 livros = [
@@ -9,55 +17,61 @@ livros = [
     {"id": 2, "titulo": "1984", "autor": "George Orwell"},
     {"id": 3, "titulo": "O Hobbit", "autor": "J.R.R. Tolkien"},
 ]
-@csrf_exempt
-def lista_livros(request):
-    return JsonResponse(livros, safe=False)
 
-@csrf_exempt
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def lista_livros(request):
+    return Response(livros)
+
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
 def detalhe_livro(request, id):
+    
     for livro in livros:
         if livro["id"] == id:
-            return JsonResponse(livro)
+            return Response(livro)
     
-    return JsonResponse({"erro": "Livro não encontrado"}, status=404)
+    return Response({"erro": "Livro não encontrado"}, status=404)
 
 #criação de livros
-@csrf_exempt
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
 def criar_livro(request):
-    if request.method == "POST":
-        data = json.loads(request.body)
 
-        novo_livro = {
-            "id": len(livros) + 1,
-            "titulo": data.get("titulo"),
-            "autor": data.get("autor"),
-        }
+    novo_livro = {
+        "id": len(livros) + 1,
+        "titulo": request.data.get("titulo"),
+        "autor": request.data.get("autor"),
+    }
 
-        livros.append(novo_livro)
+    livros.append(novo_livro)
 
-        return JsonResponse(novo_livro, status=201)
+    return Response(novo_livro, status=201)
     
 #Atualizar livros
-@csrf_exempt
+@api_view(['PUT'])
+@permission_classes([IsAuthenticated])
 def atualizar_livro(request, id):
-    if request.method == "PUT":
-        data = json.loads(request.body)
 
-        for livro in livros:
-            if livro["id"] == id:
-                livro["titulo"] = data.get("titulo", livro["titulo"])
-                livro["autor"] = data.get("autor", livro["autor"])
+    for livro in livros:
+        if livro["id"] == id:
+            livro["titulo"] = request.data.get("titulo", livro["titulo"])
+            livro["autor"] = request.data.get("autor", livro["autor"])
 
-                return JsonResponse(livro)
+            return Response(livro)
 
-        return JsonResponse({"erro": "Livro não encontrado"}, status=404)
+    return Response({"erro": "Livro não encontrado"}, status=404)
  
 #Excluir livros
-@csrf_exempt   
+@api_view(['DELETE']) 
+@permission_classes([IsAuthenticated]) 
 def deletar_livro(request, id):
-    if request.method == "DELETE":
-        global livros
+    global livros
 
-        livros = [livro for livro in livros if livro["id"] != id]
+    livros = [
+        livro for livro in livros 
+            if livro["id"] != id
+        ]
 
-        return JsonResponse({"mensagem": "Livro removido com sucesso"})
+    return Response({"mensagem": "Livro removido com sucesso"})
