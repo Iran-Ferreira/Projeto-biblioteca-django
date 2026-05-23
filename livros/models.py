1e076fbd1c923525a84c23ce2c795c36f124d4a8
Iran-Ferreira <iranferreira712@gmail.com>
2026-05-23 19:55:18 -0300

    feat: models livros

--- livros/models.py
+++ livros/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class Livro(models.Model):
+
+    titulo = models.CharField(max_length=200)
+
+    autor = models.CharField(max_length=100)
+
+    descricao = models.TextField(blank=True)
+
+    ano_publicacao = models.IntegerField()
+
+    disponivel = models.BooleanField(default=True)
+
+    criado_em = models.DateTimeField(auto_now_add=True)
+
+    atualizado_em = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self):
+        return self.titulo
