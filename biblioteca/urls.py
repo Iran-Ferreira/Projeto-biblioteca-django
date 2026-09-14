3642726ef072653fe93c7ae66792b070a50fd0f1
Iran-Ferreira <iranferreira712@gmail.com>
2026-09-13 21:50:53 -0300

    mudando rota

--- biblioteca/urls.py
+++ biblioteca/urls.py
@@ -16,14 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from .views import home
+from views import home
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
 
 urlpatterns = [
-    path('', home),
+    path('', home, name='index'),
     path('admin/', admin.site.urls),
     
     path('api/', include('livros.urls')),
